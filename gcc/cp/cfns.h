35b1c5c7fb6699541b35cd0fd6414d08b9af5ebe
Doug Gilbert <dgilbert@us.ibm.com>
2017-10-06 09:34:44 -0500

    unsupported insn for bswap emitted

--- gcc/cp/cfns.h
+++ gcc/cp/cfns.h
@@ -52,7 +52,7 @@ __inline
 #endif
 static unsigned int hash (const char *, unsigned int);
 #ifdef __GNUC__
-//__inline
+__inline
 #endif
 const char * libc_name_p (const char *, unsigned int);
 /* maximum key range = 391, duplicates = 0 */
